27e4fb8718de58fd68959cdd286cecfb88e058e3
ivan.canello <ivan.canello@telenetgroup.be>
2023-08-10 05:07:30 +0200

    updating dist and config changes

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cors())
 app.use('/', commentRoute)
 
 // Create port
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
   console.log('Server is listening to port ' + port)
 })
